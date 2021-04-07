23cd91ec38784c46e3ce85f9051a5bc2d390410e
Marco <marcosinisa.mihajlovic@gmail.com>
2021-04-07 13:42:46 +0200

    Milestone 2
    Completato

--- js/script.js
+++ js/script.js
@@ -4,20 +4,53 @@ var app = new Vue ({
     data: {
         films: [],
         search: "",
+        languages: ['it', 'en', 'jp', 'cn', 'es', 'de'],
     },
 
     methods: {
-        getMovies: function() {
+        getMoviesAndTV: function() {
             this.films = [];
             axios
-                .get('https://api.themoviedb.org/3/search/movie?api_key=5e2f325369ba7c109a4926654b75409f&query=' + this.search)
+                .get('https://api.themoviedb.org/3/search/movie?api_key=5e2f325369ba7c109a4926654b75409f&language=it-IT&query=' + this.search)
                 .then((request) => {
                     request.data.results.forEach(element => {
                         this.films.push(element);
                     })
-                    console.log(this.films);
                 })
+            
+            axios
+                .get('https://api.themoviedb.org/3/search/tv?api_key=5e2f325369ba7c109a4926654b75409f&language=it-IT&query=' + this.search)
+                .then((request) => {
+                    request.data.results.forEach(element => {
+                        this.films.push(element);
+                    })
+                })
+        },
+
+        getRate: function(vote) {
+            return parseInt(vote/2);
+        },
+
+        getRate2: function(vote) {
+            vote = parseInt(vote/2);
+            var empty = 0;
+            for (var i = 0; (i + vote) < 5; i++) {
+                empty++;
+            }
+            return empty;
+        },
+
+        hasFlag: function(lan) {
+            if(this.languages.includes(lan)) {
+                return true;
+            }
+
+            return false;
         }
+    },
+
+    created () {
+
     }
 })
 
